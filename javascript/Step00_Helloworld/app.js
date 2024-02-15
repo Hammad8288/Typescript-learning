e45391ffcd98e7a79cd3c8d56d835fbdb7a5027b
Hammad8288 <159915404+Hammad8288@users.noreply.github.com>
2024-02-15 21:26:49 +0500

    2nd

--- javascript/Step00_Helloworld/app.js
+++ javascript/Step00_Helloworld/app.js
@@ -1,10 +1,3 @@
 "use strict";
 let message = "hello world";
 console.log(message);
-let a = "hammAd ahmED";
-console.log(a.toLowerCase());
-console.log(a.toUpperCase());
-// console.log("Slicing", a.slice(0,5))
-// console.log("charat", a.charAt(0))7
-let titleCase = a.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(" ");
-console.log(titleCase);
